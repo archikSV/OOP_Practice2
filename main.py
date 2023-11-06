debeffe14f5e9d0c465cc541fe791ef8f131cba8
SalchukArtem <archik.sv@gmail.com>
2023-11-06 18:34:22 +0200

    Added Polimorfism tasks to main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 print("Task X:\n")
 
 """
-
+Polimorfism - означає можливість об'єктів різних типів вести себе однаково в специфічних контекстах.
 """
 
-# classes
+class Animal:
+    def make_sound(self):
+        pass
 
+class Dog(Animal):
+    def make_sound(self):
+        print("Woof")
 
-# objects
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow")
 
+class Bird(Animal):
+    def make_sound(self):
+        print("Tweet")
 
-# main
+def animal_sounds(animals):
+    for animal in animals:
+        animal.make_sound()
 
+dog, cat, bird = Dog(), Cat(), Bird()
+
+animals = [dog, cat, bird]
+
+animal_sounds(animals)
